3721769768ec782d646317b6d60567cec0291c1a
Braden Dubois <drab3n@gmail.com>
2020-06-11 10:23:56 -0600

    Added tests of summed variable outcome probability, fixed error in computation

--- ProPyBilityRunner.py
+++ ProPyBilityRunner.py
@@ -11,30 +11,28 @@ from probability_structures.ProPyBilityTables import *
 from regression_tests.RegressionTesting import *
 
 import os               # Used to list directory contents to select graphs
-
+from config.config_mgr import access
 
 ## TODO - Improve the actual relaying of whether the tests passed/failed, whether to proceed/stop
-# Run all regression tests first
-results = validate()
 
-if not results[0]:
-    print("Regression tests failed.")
-    print(results)
-    exit(0)
+# If set, run any tests before starting up
+if access("runRegressionTestsOnLaunch"):
+    results = validate()
 
-print(results)
+    if access("outputRegressionResults"):
+        print(results)
 
-# Default Graph Folder
-graph_file_folder = "causal_graphs"
+    if not results[0] and access("exitIfRegressionFailure"):
+        exit(-1)
 
 # Does the directory of graphs exist?
-if os.path.isdir(graph_file_folder):
+if os.path.isdir(access("graph_file_folder")):
 
     # The default file to look for
     graph_file = "causal_graph.json"
 
     # Find all JSON files in that directory
-    files = sorted([file_name for file_name in os.listdir(graph_file_folder) if file_name.endswith(".json")])
+    files = sorted([file_name for file_name in os.listdir(access("graph_file_folder")) if file_name.endswith(".json")])
 
     # Only one file, just select it
     if len(files) == 1:
@@ -42,18 +40,22 @@ if os.path.isdir(graph_file_folder):
 
     # Multiple files, list them and get a selection
     else:
-        print("Files located in:", graph_file_folder)
+        print("Files located in:", access("graph_file_folder"))
         for file_index in range(len(files)):
             print("  ", str(file_index+1) + ")", files[file_index])
+        print("  ", str(len(files)+1) + ") Exit")
 
-        file = input("Selection: ")
-        while not file.isdigit() and 1 <= int(file) <= len(files):
-            file = input("Selection: ")
+        selection = input("Selection: ")
+        while not selection.isdigit() and 1 <= int(selection) <= len(files) + 1:
+            selection = input("Selection: ")
 
-        graph_file = files[int(file)-1]
+        if selection == str(len(files)+1):
+            exit(0)
+
+        graph_file = files[int(selection) - 1]
 
     print("\nLoading:", graph_file, "\n")
-    CG = CausalGraph(graph_file_folder + "/" + graph_file)
+    CG = CausalGraph(access("graph_file_folder") + "/" + graph_file)
 
 # No directory, load whatever default is specified in the Causal Graph
 else:
