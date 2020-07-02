74faa0890f0a04dac97c4416c71b886a02e4d20a
Braden <drab3n@gmail.com>
2020-07-02 14:37:04 -0600

    Added Simulation graph, all tests, outcome generator

---
+++ diagnostic_work/generate_all_outcomes_test.py
@@ -0,0 +1,25 @@
+
+import itertools
+from probability_structures.CausalGraph import *
+
+vars = [["a", "~a"], ["b", "~b"], ["c", "~c"]]
+
+outcomes = itertools.product(*vars)
+
+tests = []
+
+cg = CausalGraph("causal_graphs/simulation.json")
+
+for outcome in outcomes:
+    arg = "A = " + outcome[0] + ", B = " + outcome[1] + ", C = " + outcome[2]
+    tests.append({
+        "name": "Probability Test: P(" + "".join(outcome) + ")",
+        "type": "probability",
+        "args": [
+            arg
+        ],
+        "expected_result": cg.probability(parse_outcomes_and_interventions(arg), [])
+    })
+
+with open("test_file", "w") as f:
+    json.dump({"tests": tests}, f)
