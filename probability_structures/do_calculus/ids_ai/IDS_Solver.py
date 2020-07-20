4f379a1f98f53d4bc3360964c701c6feed316cf1
Braden <drab3n@gmail.com>
2020-07-20 17:04:28 -0600

    Support unobservable variables in IDS Solver

--- probability_structures/do_calculus/ids_ai/IDS_Solver.py
+++ probability_structures/do_calculus/ids_ai/IDS_Solver.py
@@ -9,11 +9,12 @@
 
 # Basic IDS-approach solver taking an initial query and solving until there are no interventions left
 #   Thanks, Professor Horsch, for CMPT 317!
-
+from probability_structures.do_calculus.application.CustomSetFunctions import clean
 from probability_structures.do_calculus.application.QueryStructures import QueryList, Query, QueryBody
 from probability_structures.do_calculus.application.DoCalculusQueryOptions import do_calculus_options
 from probability_structures.do_calculus.ids_ai.Stack import Stack
 from probability_structures.Graph import Graph
+from util.IO_Logger import io
 
 
 class Solution:
@@ -48,18 +49,22 @@ class IDSSolver:
     A basic iterative-deepening-search approach to do-calculus, reducing some query y | do(x) see(w) to only observation
     """
 
-    def __init__(self, graph: Graph, y: set, x: set, w: set):
+    def __init__(self, graph: Graph, y: set, x: set, w: set, u=None):
         """
         Create an iterative-deepening-search solver to manipulate the given sets until there are no interventions left.
         :param graph: A Graph object representing the graph space
         :param y: The outcomes we wish to measure, as a set of strings
         :param x: All interventions, as a set of strings
         :param w: All observations, as a set of strings
+        :param u: All unobservable variables, as a set of strings
         """
         self.graph = graph.copy()
         self.y = y
         self.x = x
         self.w = w
+
+        # Either store u, or create an empty set
+        self.unobservable = u if u else set()
 
         self.initial_query_list = QueryList([Query(self.y.copy(), QueryBody(self.x.copy(), self.w.copy()))])
 
@@ -71,9 +76,15 @@ class IDSSolver:
         :return:
         """
 
+        # Used to prevent wasteful / unfruitful paths to go down; especially useful with depth increase
+        seen = set()
+
         # We will go to a depth maximum 10, starting with 1, and increasing until we find it.
         maximum_depth = 10
         current_max_depth = 1
+
+        # Test to see how many queries are actually caught by the "seen" set
+        t = 0
 
         while current_max_depth <= maximum_depth:
 
@@ -84,15 +95,28 @@ class IDSSolver:
             self.stack.clear()
             self.stack.push((self.initial_query_list.copy(), 1, []))
 
+            # Clear the "seen"
+            seen.clear()
+
             while not self.stack.empty():
 
                 # Pop the current item in the IDS stack and see if it's our goal
                 current, item_depth, history = self.stack.pop()
                 if self.goal(current):
+                    print("Bounced", str(t), "duplicate states.")
                     return Solution(True, history, current)
 
+                # String representation so as to not repeat an unnecessary path
+                str_rep = str(current)
+
+                if str_rep in seen:
+                    t += 1
+
                 # Unpack the item and see if we can push all its resulting options
-                if item_depth < current_max_depth:
+                if item_depth < current_max_depth and str_rep not in seen:
+
+                    # Add the string to ensure we don't compute all this again for an equivalent query
+                    seen.add(str_rep)
 
                     # Generate all new options
                     all_options = do_calculus_options(current, self.graph)
@@ -102,6 +126,9 @@ class IDSSolver:
 
                         # As unpacked above, this is a tuple (query state, depth, history)
                         self.stack.push((option[1], item_depth+1, history + [option[0]]))
+
+            # Couldn't find an answer at this depth
+            io.write("Couldn't find a solution at depth:", current_max_depth, "Looked at:", len(seen), "queries.")
 
             # Increase the depth and run again
             current_max_depth += 1
@@ -116,4 +143,15 @@ class IDSSolver:
         :param x: A QueryList to check
         :return: True if this QueryList is a valid Goal, False otherwise
         """
-        return x.no_interventions()
+        # Ensure there are no more "do's"
+        if not x.no_interventions():
+            return False
+
+        # Ensure that any unobservable variables are not used in the final solution
+        for query in x.queries:                 # Check each query
+            if isinstance(query, Query):        # Filter out Sigma's
+                all_used = clean(query.head) | clean(query.body.interventions) | clean(query.body.observations)
+                if len(all_used & self.unobservable) != 0:
+                    return False
+
+        return True
