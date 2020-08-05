69b1cbe53e27894e66f5eaca8676151ceac40e5b
Braden <drab3n@gmail.com>
2020-08-05 16:40:00 -0600

    API cleanup

--- src/API.py
+++ src/API.py
@@ -15,8 +15,11 @@ class API:
 
     def __init__(self):
 
-        # Just parse out and
         self.parsed = None
+        self._variables = None
+        self._outcomes = None
+        self._tables = None
+        self._graph = None
 
     def fullscreen(self):
         webview.windows[0].toggle_fullscreen()
@@ -29,9 +32,6 @@ class API:
         with open(filename, 'w') as f:
             f.write(content)
 
-    def ls(self):
-        return os.listdir('.')
-
     def access(self, param):
         return access(param)
 
@@ -39,24 +39,30 @@ class API:
         return sorted([f for f in os.listdir(PREFIX + self.access("graph_file_folder")) if f.endswith(".json")])
 
     def all_variable_data(self):
-        variables = self.parsed["variables"]
-        return [[i, variables[i].outcomes, variables[i].parents] for i in sorted(variables)]
+        return [[i, self._variables[i].outcomes, self._variables[i].parents] for i in sorted(self._variables)]
 
-    def variable_table(self, variable: str):
-        t = self.parsed["tables"][variable]
-        return [[variable, t.given, "P"], *[[row[0].outcome, [x.outcome for x in row[1]], row[2]] for row in t.table_rows]]
+    def variable_table(self, v: str):
+        t = self._tables[v]
+        return [[v, t.given, "P"], *[[row[0].outcome, [x.outcome for x in row[1]], row[2]] for row in t.table_rows]]
 
     def load_file(self, graph_file):
+
         self.parsed = parse_graph_file_data(PREFIX + self.access("graph_file_folder") + "/" + graph_file)
-        # print(graph_file, "loaded.")
+
+        # Unpack the main contents of the parsed file for easier access
+        self._variables = self.parsed["variables"]
+        self._outcomes = self.parsed["outcomes"]
+        self._tables = self.parsed["tables"]
+        self._graph = self.parsed["graph"]
+
         return True
 
     def variables(self):
-        return sorted([k for k in self.parsed["variables"]])
+        return sorted([k for k in self._variables])
 
     def outcomes(self, variable):
-        return self.parsed["variables"][variable].outcomes
+        return self._outcomes[variable]
 
     def outcome_dict(self):
-        return {k: self.outcomes(k) for k in self.parsed["variables"]}
+        return {k: self._outcomes[k] for k in self._variables}
 
