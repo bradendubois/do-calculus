81c24fc22d1dfdeaa35f828f67c452e743efdeef
Braden Dubois <drab3n@gmail.com>
2021-03-27 18:13:51 -0600

    Moved print-test-result function

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -13,17 +13,3 @@ json_model = graphs / "test.json"
 cg = CausalGraph(**parse_model(test_file))
 graph = cg.graph
 bc = BackdoorController(graph)
-
-
-def print_test_result(success: bool, msg: str):
-    """
-    Print a test result to standard out, with a header marking the success of the test
-    @param success: bool; True if the test was successful, False otherwise
-    @param msg: string; Any arbitrary message returned by the test
-    """
-    green = '\033[92m'
-    fail = '\033[91m'
-    end = '\033[0m'
-
-    color = green if success else fail
-    print(f"[{color}{'OK' if success else 'ERROR'}{end}]: {msg}")
